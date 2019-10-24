fdc388085fdd8d8e090d0070bac972921fc8940f
Shreyas Patankar <sgpatankar@berkeley.edu>
2019-10-23 22:23:07 -0700

    init

---
+++ Project Lego/Project Lego/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project Lego
+//
+//  Created by Shreyas Patankar on 10/23/19.
+//  Copyright © 2019 Lego. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
